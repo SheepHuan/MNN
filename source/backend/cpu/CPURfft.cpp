84fdd6b8cfbb2f18044d2638a3c561d5c9c9dc0d
SheepHuan <justno@foxmail.com>
2024-04-22 04:54:55 +0000

    240422：实现了朴素的FFT变换

--- source/backend/cpu/CPURfft.cpp
+++ source/backend/cpu/CPURfft.cpp
@@ -1,30 +1,101 @@
+/*
+ * @Author: Huan Yang
+ * @Date: 2024-04-22 01:26:16
+ * @LastEditors: Huan Yang
+ * @LastEditTime: 2024-04-22 04:45:41
+ * @FilePath: /MNN/source/backend/cpu/CPURfft.cpp
+ * @Description:
+ *
+ * Copyright (c) 2024 by Huan Yang, All Rights Reserved.
+ */
 #include "backend/cpu/CPURfft.hpp"
 #include "backend/cpu/CPUBackend.hpp"
 #include "core/Macro.h"
+#include <complex>
+#include <cmath>
 
 namespace MNN
 {
+    void make_euler_weights(int outSize, int signSize, float *realWeights, float *imagWeights)
+    {
+        return;
+    }
+
     CPURfft::CPURfft(Backend *b) : Execution(b)
     {
     }
     ErrorCode CPURfft::onResize(const std::vector<Tensor *> &inputs, const std::vector<Tensor *> &outputs)
     {
-        // 要根据输入的形状，在这里预先计算欧拉权重？
-
-        // 确定rfft的计算维度、输入大小、输出大小
+        // 确定参数
+        int *slPtr = inputs[1]->host<int>();
         int *tdPtr = inputs[2]->host<int>();
-        int targetDim = tdPtr[0];
-        int inDimSize = inputs[0]->buffer().dim[targetDim].extent;
-        int outDimSize = inDimSize / 2 + 1;
 
+        this->_signalLength = slPtr[0];
+        this->_computeDim = tdPtr[0];
+        this->_computeDimSize = inputs[0]->buffer().dim[this->_computeDim].extent;
+        this->_resultDimSize = int(this->_computeDimSize / 2) + 1;
 
+        // 这里要将mem值为零。
 
         return NO_ERROR;
     }
 
     ErrorCode CPURfft::onExecute(const std::vector<Tensor *> &inputs, const std::vector<Tensor *> &outputs)
     {
+        // NOTE: 这个实现只支持3D输入[n,f,s]
+        if (this->implementMode == 0)
+        {
+            if (inputs[0]->buffer().dimensions == 3)
+            {
+                int batch = inputs[0]->buffer().dim[0].extent;
+                int frameLen = inputs[0]->buffer().dim[1].extent;
+                int signalLen = inputs[0]->buffer().dim[2].extent;
+                float *realPtr = outputs[0]->host<float>();
+                float *imagPtr = outputs[1]->host<float>();
+                float *inputPtr = inputs[0]->host<float>();
+                if (this->_resultDimSize == -1)
+                {
+                    int *slPtr = inputs[1]->host<int>();
+                    int *tdPtr = inputs[2]->host<int>();
 
+                    this->_signalLength = slPtr[0];
+                    this->_computeDim = tdPtr[0];
+                    this->_computeDimSize = inputs[0]->buffer().dim[this->_computeDim].extent;
+                    this->_resultDimSize = int(this->_computeDimSize / 2) + 1;
+                }
+                for (int i = 0; i < batch; i++)
+                {
+                    for (int j = 0; j < frameLen; j++)
+                    {
+
+                        for (int k = 0; k < this->_resultDimSize; k++)
+                        {
+                            int index = i * frameLen * this->_resultDimSize + j * this->_resultDimSize + k;
+                            realPtr[index] = 0.0;
+                            imagPtr[index] = 0.0;
+                            for (int n = 0; n < signalLen; n++)
+                            {
+                                int inputIdx = i * frameLen * signalLen + j * signalLen + n;
+                                double angle = -2.0 * M_PI * k * n / signalLen;
+                                double realWeight = cos(angle);
+                                double imagWeight = sin(angle);
+                                // outputs?
+                                realPtr[index] += inputPtr[inputIdx] * realWeight;
+                                imagPtr[index] += inputPtr[inputIdx] * imagWeight;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return ErrorCode::INPUT_DATA_ERROR;
+            }
+        }
+        else
+        {
+            return ErrorCode::NOT_SUPPORT;
+        }
         return NO_ERROR;
     }
 
--- source/backend/cpu/CPURfft.hpp
+++ source/backend/cpu/CPURfft.hpp
@@ -1,3 +1,13 @@
+/*
+ * @Author: Huan Yang
+ * @Date: 2024-04-22 01:26:16
+ * @LastEditors: Huan Yang
+ * @LastEditTime: 2024-04-22 02:47:29
+ * @FilePath: /MNN/source/backend/cpu/CPURfft.hpp
+ * @Description:
+ *
+ * Copyright (c) 2024 by Huan Yang, All Rights Reserved.
+ */
 #ifndef CPURfft_hpp
 #define CPURfft_hpp
 
@@ -6,6 +16,8 @@
 
 namespace MNN
 {
+    void make_euler_weights(int outSize, int signSize, float *realWeights, float *imagWeights);
+
     class CPURfft : public Execution
     {
     public:
@@ -16,7 +28,13 @@ namespace MNN
 
     protected:
         bool mSupportMultiThread = false;
+        int implementMode = 0;
 
+    private:
+        int _signalLength = -1;
+        int _computeDim = -1;     // 要处理输入张量的哪一个维度
+        int _computeDimSize = -1; // 处理输入张量的计算维度的大小
+        int _resultDimSize = -1;  // 输出张量计算维度的大小
     };
 }
 #endif
